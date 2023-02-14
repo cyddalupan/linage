bac9e88564bbd5d094afc422938bcbb65e58455f
Cyd Dalupan <cydmdalupan@gmail.com>
2023-02-14 11:12:37 +0800

    Wiki Database

--- lineage/settings.py
+++ lineage/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Installs
-    'bootstrap5',
-    'django_bootstrap_icons',
     # Created
     'wiki.apps.WikiConfig',
 ]
--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class WikiContent(models.Model):
+    title = models.CharField(unique=True, max_length=255)
+    tags = models.CharField(max_length=520)
+    content = models.TextField()    
+    folder_id = models.IntegerField()
+    created_by = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class WikiFolders(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    folder_id = models.IntegerField()
+    created_by = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class WikiContentArchive(models.Model):
+    title = models.CharField(unique=True, max_length=255)
+    tags = models.CharField(max_length=520)
+    content = models.TextField()    
+    folder_id = models.IntegerField()
+    created_by = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
